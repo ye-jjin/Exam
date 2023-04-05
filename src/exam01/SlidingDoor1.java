c76ceaa3e0f1977695d3156e113868919ecee083
lyj <lyj@rwk>
2023-04-05 16:41:20 +0900

    second commit

--- src/exam01/SlidingDoor1.java
+++ src/exam01/SlidingDoor1.java
@@ -1,184 +1,108 @@
 package exam01;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class SlidingDoor1 {
+	static int row;
+	static int col;
+	static int findNum;
+	static int chRow;
+	static int chCol;
+	static int temp;
+	static int count;
+	
+	//초기 2차원 배열
+	private static int arrStart[][] = {{1,2,3},
+									   {4,5,6},
+									   {7,8,0}};	
+	
+	private static int arrEnd[][];
+
+	// arrEnd에서 0의 위치를 확인하기
+	public static int moveNum(int arrEnd[][],int arrStart[][],int row,int col, int findNum,int chRow, int chCol) {
+		
+		count++;
+		
+		for(int i=0; i<arrEnd.length; i++){
+            for(int j=0; j<arrEnd[i].length; j++){
+                if( arrEnd[i][j] == 0) {
+                	row = i;
+                	col = j;
+                	System.out.println("arrEnd에서 0의 위치: ["+row+"] , ["+col+"]");
+                	
+                	// arrStart의 [row][col] 번째에 위치한 findNum 찾기
+                	findNum=arrStart[row][col];
+                	System.out.println("arrStart의 ["+row+"]["+col+"] 번째에 위치한 수:"+findNum);      	
+                }
+            }
+        }
+		
+		//arrEnd에서 findNum이 위치하고 있는 행과 열 확인하기
+		for(int i=0; i<arrEnd.length; i++){
+			for(int j=0; j<arrEnd[i].length; j++){
+				if( arrEnd[i][j] == findNum) {
+					chRow = i;
+					chCol = j;               	      	
+				}
+			}
+		}
+		System.out.println("arrEnd에서 findNum이 위치하고 있는 위치:["+chRow+"] , ["+chCol+"]");
+		
+		// arrEnd[row][col] 값과 arrEnd[chRow][chCol] 값 바꾸기
+		temp = arrEnd[row][col];
+	   	arrEnd[row][col] = arrEnd[chRow][chCol];
+	   	arrEnd[chRow][chCol] = temp;
+	   	System.out.println("변경값 확인:"+arrEnd[row][col]);
+	   	
+	   	
+	   	//이동 후 배열 출력하기-확인용
+	   	
+	   	for (int i = 0; i < arrEnd.length; i++) {
+	           int inArr[] = arrEnd[i];
+	           for (int j = 0; j < inArr.length; j++) {
+	               System.out.print(inArr[j] + " ");
+	           }
+	        System.out.println(); //->배열의 공간 할당
+	    }	   	
+	   	
+	   	
+		return count; 
+	}	
+	
 
 	public static void main(String[] args) {
 		
-		//초기 2차원 배열
-		int arrStart[][] = {{1,2,3},
-							{4,5,6},
-							{7,8,0}};
+		// 1.스캐너생성
+		Scanner scan = new Scanner(System.in);
+		
+		// 2.만들 배열의 행/열 수 입력 받기
+		int num = scan.nextInt(); //->3
+		
+		// 3.입력 받은 행/열 수로 2차원 배열 공간 만들기
+		int arrEnd[][]= new int[num][num]; //[3][3] 배열 생성
 		
 		
-		//-> 테스트 위해서 여기까지 주석으로 막고 임의의 입력받은 배열 고정하기
-		int arrEnd[][] = {{1,0,3},
-						 {4,2,5},
-						 {7,8,6}};
-		
-		
-		// 6.입력받은 배열에 0 숫자가 들어있는 위치 확인하기 -> 행과 열 확인가능
-		int row = 0,col=0;
-		int findNum = 0;
-		
+		// 4.입력받은 예제 값을 배열에 넣기
 		for(int i=0; i<arrEnd.length; i++){
             for(int j=0; j<arrEnd[i].length; j++){
-                if( arrEnd[i][j] == 0) {
-                	row = i;
-                	col = j;
-//                	System.out.println("["+row+"] , ["+col+"]"); //->[0,1]
-                	
-                	// 7. 최종 배열 0의 자리에 해당하는 초기 배열의 값 찾기 
-                	findNum=arrStart[row][col];
-//                	System.out.println(findNum); //-> 2
-                	      	
-                }
+                arrEnd[i][j] = scan.nextInt();
             }
-        }
-		
-		System.out.println("입력받은 최종 배열의 0의 숫자가 들어있는 위치:["+row+"] , ["+col+"]"); 
-		System.out.println("초기 배열의 [row][col] 자리에 해당하는 초기 값:"+findNum); 
-		
-		// 8. 최종 배열에서 찾은 값(findNum)이 위치하고 있는 행과 열 확인하기 arrEnd[chRow][chCol]
-		int chRow = 0,chCol = 0;
-		
-		for(int i=0; i<arrEnd.length; i++){
-            for(int j=0; j<arrEnd[i].length; j++){
-                if( arrEnd[i][j] == findNum) {
-                	chRow = i;
-                	chCol = j;               	      	
-                }
-            }
-        }
-       	System.out.println("최종 배열에서 찾은 값이 위치하고 있는 위치:["+chRow+"] , ["+chCol+"]"); //->[1,1]
-       	
-       	// 9. 최종배열에서 [row,col]자리의 값과 [chRow,chCol]자리의 값을 바꾸기   
-       	int temp = arrEnd[row][col];
-       	arrEnd[row][col] = arrEnd[chRow][chCol];
-       	arrEnd[chRow][chCol] = temp;
-       	System.out.println("변경값 확인:"+arrEnd[row][col]); 
-		
+        }		
+				
+		// 5. 위의 함수 반복해서 실행하기
+		boolean result=true;
+		while(result) {			
+			moveNum(arrEnd, arrStart, row, col, findNum,chRow,chCol);
+			
+			if(Arrays.deepEquals(arrStart, arrEnd)) {
+				result=false; //break문
+			}			
+			System.out.println("최종이동횟수:"+count);
+			System.out.println("====================================");
+		}
 
-		// 10. 한번 이동한 후 변경된 arrEnd 2차원 배열 확인해보기
-       	System.out.println("=== 한번 이동한 후 배열 출력 ===");
-       	for (int i = 0; i < arrEnd.length; i++) {
-               int inArr[] = arrEnd[i];
-               for (int j = 0; j < inArr.length; j++) {
-                   System.out.print(inArr[j] + " ");
-               }
-            System.out.println(); //->배열의 공간 할당
-        }
-       	
-       	// 다시 반복
-       	// 11. 한번 이동한 배열에서의 0의 행/열 찾기
-       	//System.out.println("숫자 0 위치의 행과 열: ["+chRow+"], ["+chCol+"]"); //[1][1]
-       	
-       	for(int i=0; i<arrEnd.length; i++){
-            for(int j=0; j<arrEnd[i].length; j++){
-                if( arrEnd[i][j] == 0) {
-                	row = i;
-                	col = j;
-                	//System.out.println("["+row+"] , ["+col+"]"); //->[1,1]
-                	
-                	// 12. 초기 배열에서의 위의 행/열에서의 값 찾기 
-                	findNum=arrStart[row][col];
-                	//System.out.println(findNum); //-> 5
-                	      	
-                }
-            }
-        }
-       	
-       	System.out.println("타깃 배열의 0의 숫자가 들어있는 위치:["+row+"] , ["+col+"]"); 
-		System.out.println("초기 배열의 [row][col] 자리에 해당하는 초기 값:"+findNum); 
-       	
-       	// 13. 한번 이동한 배열에서의 찾은 타깃(findNum)값이 위치하고 있는 행/열 찾기
-       	for(int i=0; i<arrEnd.length; i++){
-            for(int j=0; j<arrEnd[i].length; j++){
-                if( arrEnd[i][j] == findNum) {
-                	chRow = i;
-                	chCol = j;               	      	
-                }
-            }
-        }
-       	System.out.println("한번 이동한 배열에서 찾은 값이 위치하고 있는 위치:["+chRow+"] , ["+chCol+"]"); //->[1,2]
-       	
-       	
-       	// 14. 타깃배열에서 [row,col]자리의 값과 [chRow,chCol]자리의 값을 바꾸기
-    	temp = arrEnd[row][col];
-       	arrEnd[row][col] = arrEnd[chRow][chCol];
-       	arrEnd[chRow][chCol] = temp;
-       	System.out.println("변경값 확인:"+arrEnd[row][col]);
-       	
-       	
-       	// 15. 두번 이동한 후 2차원 배열 확인해보기
-       	System.out.println("=== 두번 이동한 후 배열 출력 ===");
-       	for (int i = 0; i < arrEnd.length; i++) {
-               int inArr[] = arrEnd[i];
-               for (int j = 0; j < inArr.length; j++) {
-                   System.out.print(inArr[j] + " ");
-               }
-            System.out.println(); //->배열의 공간 할당
-        }
-       	
-       	// 다시 반복
-       	// 16. 두번 이동한 배열에서의 0의 행/열 찾기
-       	for(int i=0; i<arrEnd.length; i++){
-            for(int j=0; j<arrEnd[i].length; j++){
-                if( arrEnd[i][j] == 0) {
-                	row = i;
-                	col = j;
-                	//System.out.println("["+row+"] , ["+col+"]"); //->[1,2]
-                	
-                	// 17. 초기 배열에서의 위의 행/열에서의 값 찾기 
-                	findNum=arrStart[row][col];
-                	//System.out.println(findNum); //-> 6
-                	      	
-                }
-            }
-        }
-       	
-       	System.out.println("타깃 배열의 0의 숫자가 들어있는 위치:["+row+"] , ["+col+"]"); 
-		System.out.println("초기 배열의 [row][col] 자리에 해당하는 초기 값:"+findNum); 
-       
-       	// 18. 두번 이동한 배열에서의 찾은 타깃(findNum)값이 위치하고 있는 행/열 찾기
-		for(int i=0; i<arrEnd.length; i++){
-            for(int j=0; j<arrEnd[i].length; j++){
-                if( arrEnd[i][j] == findNum) {
-                	chRow = i;
-                	chCol = j;               	      	
-                }
-            }
-        }
-       	System.out.println("한번 이동한 배열에서 찾은 값이 위치하고 있는 위치:["+chRow+"] , ["+chCol+"]"); //->[2,2]
-		
-       	// 19. 타깃배열에서 [row,col]자리의 값과 [chRow,chCol]자리의 값을 바꾸기
-       	
-       	temp = arrEnd[row][col];
-       	arrEnd[row][col] = arrEnd[chRow][chCol];
-       	arrEnd[chRow][chCol] = temp;
-       	System.out.println("변경값 확인:"+arrEnd[row][col]);
-       	
-       	// 20. 세번 이동한 후 2차원 배열 확인해보기
-       	
-    	System.out.println("=== 세번 이동한 후 배열 출력 ===");
-       	for (int i = 0; i < arrEnd.length; i++) {
-               int inArr[] = arrEnd[i];
-               for (int j = 0; j < inArr.length; j++) {
-                   System.out.print(inArr[j] + " ");
-               }
-            System.out.println(); //->배열의 공간 할당
-        }
-       	
-       	
-       	// 처음 배열과 최소 이동한 배열을 비교하기
-       	if(Arrays.deepEquals(arrStart,arrEnd)) {
-       		System.out.println("동일한 배열의 값을 가지고 있습니다.");
-       	}else {
-       		System.out.println("다른 배열의 값을 가지고 있습니다.");
-       	}
-       	
 	}//main
 
-}//Exam3
+}//SlidingDoor1
 
